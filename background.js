903d06fd1bac4e91d45a1ab756aa50e92177bf07
Sid <sidchou93@gmail.com>
2019-12-10 13:03:22 -0500

    load history in content page on complete

--- background.js
+++ background.js
@@ -20,6 +20,10 @@ include in manifest.json
  });
 */
 
+let t = 0;
+let pT = 0;
+let load = false;
+//calculate the lastVisitTime
 function milliConv(milli) {
 
   let t = [1, 1000, 60, 60];
@@ -27,6 +31,7 @@ function milliConv(milli) {
   for (i = 1; i < 4; i++) {
     t[i] = t[i - 1] * t[i];
   }
+  //calculate milli in array
 
   if (milli < t[1]) {
     return "now"
@@ -37,54 +42,92 @@ function milliConv(milli) {
   } else {
     return Math.round(milli / t[3]) + " hour"
   }
-}
+  //return string based on time
+};
 
-chrome.webNavigation.onCompleted.addListener(function() {
 
 
-  data();
+chrome.webNavigation.onCompleted.addListener(buttonClicked //function() {
   //triggered when page is loaded
 
-  chrome.history.search({
-    text: '',
-    maxResults: 100
-  }, function(data) {
-    data.forEach(function(page, i) {
-      console.log(i);
-      console.log(page.title);
-      console.log(page.url);
-      console.log(page.visitCount);
-      console.log(page.lastVisitTime);
-      console.log(Date.now() - page.lastVisitTime);
-      console.log(milliConv(Date.now() - page.lastVisitTime));
-
-    });
-  });
-
-}, {
-  url: [{
-    urlMatches: 'https://www.nytimes.com/'
-  }]
-});
-
-chrome.browserAction.onClicked.addListener((e) => {
-  // chrome.tabs.sendMessage( {
-  //     "message": "browser action"
-  //   }
-  // );
-
-  chrome.runtime.sendMessage(
-    {"message": "browser action"}
-    //,(m)=>console.log(m)
-  )
-
-  console.log(e);
-}
+  //   chrome.history.search({
+  //     text: '',
+  //     maxResults: 100
+  //   }, function(data) {
+  //     data.forEach(function(page, i) {
+  //       // console.log(i);
+  //       // console.log(page.title);
+  //       // console.log(page.url);
+  //       // console.log(page.visitCount);
+  //       // console.log(page.lastVisitTime);
+  //       // console.log(Date.now() - page.lastVisitTime);
+  //       // console.log(milliConv(Date.now() - page.lastVisitTime));
+  //       // console.log(page);
+  //     });
+  //   });
+  //
+  // }, {
+  //   url: [{
+  //     urlMatches: 'https://www.nytimes.com/'
+  //   }]
+  // });
+  //}
 );
 
 
-function receiver(request, sender, sendResponse) {
-  if (request.message === "thank you") {
-    // Not doing anything for messages received but I could!
+function logHistory(tab) {
+  chrome.history.search({
+    text: '',
+    maxResults: 2
+  }, function(data) {
+    data.forEach(function(page, i) {
+      // console.log(i);
+      // console.log(page.title);
+      // console.log(page.url);
+      // console.log(page.visitCount);
+      // console.log(page.lastVisitTime);
+      // console.log(Date.now() - page.lastVisitTime);
+      // console.log(milliConv(Date.now() - page.lastVisitTime));
+      // console.log(page);
+      chrome.tabs.sendMessage(tab.tabId, page);
+    });
+  });
+}
+
+
+
+// chrome.browserAction.onClicked.addListener(buttonClicked);
+// Handle that click
+function buttonClicked(tab) {
+  // Send a message to the active tab
+
+  //see if newly loaded
+  t = tab.timeStamp;
+  //check if its loading the same page(2000ms)
+  if ((t - pT) > 2000) {
+    //different/new req
+    load = true;
+  } else {
+    load = false
   }
+  if (load) {
+    logHistory(tab)
+  }
+
+
+  pT = t;
+
+
+}
+
+
+
+// Listening for messages
+chrome.runtime.onMessage.addListener(receiver);
+
+function receiver(request, sender, sendResponse) {
+  console.log("requested");
+  // if (request.message === "thank you") {
+  //   // Not doing anything for messages received but I could!
+  // }
 }
--- contentScripts.js
+++ contentScripts.js
@@ -15,14 +15,13 @@ chrome.runtime.onMessage.addListener(loadBrowserHistory);
 
 // Handle the message --> sample code from Shiffman
 function loadBrowserHistory(request, sender, sendResponse) {
-          consloe.log(request)
-    // Now if the message matches "browser action"
+          console.log(request)
+
+
+    // // Now if the message matches "browser action"
     if (request.message) {
-        // // Change the background color again
-        // var elts = document.getElementsByTagName('p');
-        // for (var i = 0; i < elts.length; i++) {
-        //     elts[i].style['background-color'] = '#F0C';
-        // }
+      console.log("responding")
+
         // // Send a message back!
         chrome.runtime.sendMessage({
             "message": "thank you"
